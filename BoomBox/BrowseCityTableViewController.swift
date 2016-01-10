3825a94ca856785dd80fa9f23db1b21880a1ebcf
Matthew McClure <matthewmcclure@gmail.com>
2016-01-10 13:44:15 -0600

    add images

--- BoomBox/BrowseCityTableViewController.swift
+++ BoomBox/BrowseCityTableViewController.swift
@@ -29,23 +29,24 @@ class BrowseCityTableViewController: UITableViewController {
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 5
     }
 
-    /*
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+        let cell = tableView.dequeueReusableCellWithIdentifier("spaceCell", forIndexPath: indexPath)
 
         // Configure the cell...
+        cell.layoutMargins = UIEdgeInsetsZero
 
         return cell
     }
-    */
+
 
     /*
     // Override to support conditional editing of the table view.
@@ -82,7 +83,7 @@ class BrowseCityTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -90,6 +91,6 @@ class BrowseCityTableViewController: UITableViewController {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
+    
 
 }
