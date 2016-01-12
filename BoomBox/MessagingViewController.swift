01227eb581cfc0cb17278f77a6521ccade9bd80a
Joao Alves <joaopaulogalvao1@gmail.com>
2016-01-12 09:14:08 -0200

    Add MessagingViewController to project.

---
+++ BoomBox/MessagingViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MessagingViewController.swift
+//  BoomBox
+//
+//  Created by Joao Paulo Galvao Alves on 1/12/16.
+//  Copyright © 2016 warriors. All rights reserved.
+//
+
+import Foundation
