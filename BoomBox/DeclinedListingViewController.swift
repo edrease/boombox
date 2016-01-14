cb1c09191869953815847957b812f9d5e87ec31f
Edrease Peshtaz <edrease@gmail.com>
2016-01-14 15:43:46 -0800

    added reservation storyboard and view controllers

---
+++ BoomBox/DeclinedListingViewController.swift
@@ -0,0 +1,33 @@
+//
+//  DeclinedListingViewController.swift
+//  BoomBox
+//
+//  Created by Edrease Peshtaz on 1/14/16.
+//  Copyright © 2016 warriors. All rights reserved.
+//
+
+import UIKit
+
+class DeclinedListingViewController: UIViewController, UITableViewDataSource {
+
+  @IBOutlet weak var tableView: UITableView!
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 3
+  }
+  
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell!
+    cell.textLabel?.text = "Declined listing"
+    return cell
+  }
+}
